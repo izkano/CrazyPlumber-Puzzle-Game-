df91c73ffc52df355b2ec81a6f0d1397f7994d6e
TACHOU andrei <andrei.tachou@etu.u-paris.fr>
2024-02-12 19:19:56 +0100

    Refactorisation du code + fixation de la taille de la fenetre

---
+++ src/exception/MapException.java
@@ -0,0 +1,10 @@
+package exception;
+
+@SuppressWarnings("serial")
+public class MapException extends Exception {
+	public MapException(String s) {
+		super(s);
+	}
+	
+	public MapException() {}
+}
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,18 +1,22 @@
 package main;
 
-import java.io.IOException;
-
 import javax.swing.JFrame;
+
+import view.GamePanel;
 
 public class Main {
     public static void main(String[] args) {
         JFrame frame = new JFrame();
         GamePanel gamePanel = new GamePanel();
+        
         frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
         frame.add(gamePanel);
+        
         frame.pack();
         frame.setVisible(true);
         gamePanel.startGameThread();
-}
+    }
 }
