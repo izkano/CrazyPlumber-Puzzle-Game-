804b88736be761325da2b9c2f7c8cfbc38368c8b
TACHOU andrei <andrei.tachou@etu.u-paris.fr>
2024-02-22 13:49:34 +0100

    Ajout de documentation Javadoc

--- src/model/State.java
+++ src/model/State.java
@@ -1,5 +1,10 @@
 package model;
 
+/**
+ * Etat du jeu : MENU, le joueur se trouve dans le menu
+ * 				 PLAYING, le joueur est en train de résoudre un niveau
+ *               PAUSE, le jeu est en pause, le joueur a appuyé sur echap
+ */
 public enum State {
 	MENU,
 	PLAYING,
