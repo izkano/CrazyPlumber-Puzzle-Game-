ed27b15a6448f4e0be50d2c7432d028bdcbf00ef
KHADIR abdessamad <abdessamad.khadir@etu.u-paris.fr>
2024-05-07 16:11:11 +0200

    réglage d'un bug sur l'implémentation de la sauvgarde

--- src/model/Map.java
+++ src/model/Map.java
@@ -9,6 +9,7 @@ import exception.MapException;
 import javax.imageio.ImageIO;
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.RandomAccessFile;
 import java.io.IOException;
 
 
