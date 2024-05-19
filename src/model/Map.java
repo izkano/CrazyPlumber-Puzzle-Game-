c106b73dac5e8f0bcbf2d293d573de157d6941f1
SEMANI issac <issac.semani@etu.u-paris.fr>
2024-05-19 18:14:34 +0200

    etoiles

--- src/model/Map.java
+++ src/model/Map.java
@@ -18,8 +18,8 @@ import java.util.Random;
 public class Map {
 
 	private int level;
-	public int minimumMoves;
-	public int playerMoves;
+	private int minimumMoves;
+	private int playerMoves;
 
 	private Cell[][] start;
 	private final ArrayList<int[]> first;
@@ -85,8 +85,36 @@ public class Map {
 
 	// Méthode pour calculer ou assigner le nombre minimum de coups
     private int calculateMinimumMoves(int level) {
-        return 10; 
-    }
+		switch (level) {
+			case 1:
+				return 9;
+			case 2:
+				return 2;
+			case 3:
+				return 3;
+			case 4:
+				return 4;
+			case 5:
+				return 5;
+			case 6:
+				return 6;
+			case 7:
+				return 7;
+			case 8:
+				return 8;
+			case 9:
+				return 9;
+			case 10:
+				return 10;
+			case 11:
+				return 11;
+			case 12:
+				return 12;
+			default:
+				throw new IllegalArgumentException("Invalid level: " + level);
+		}
+	}
+	
 	public int getMinimumMoves() {
 		return minimumMoves;
 	}
